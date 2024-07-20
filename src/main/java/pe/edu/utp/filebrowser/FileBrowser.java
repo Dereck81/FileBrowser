5f06529a680a0c21f6537202b93525ed14866aaa
Dereck <derecknova23@gmail.com>
2024-07-20 01:48:20 -0500

    Bug fix
    TODO: Fix error when moving or copying files to directories where they have a file with the same name (generates conflicts) and block options or improve search location

--- src/main/java/pe/edu/utp/filebrowser/FileBrowser.java
+++ src/main/java/pe/edu/utp/filebrowser/FileBrowser.java
@@ -3,6 +3,7 @@ package pe.edu.utp.filebrowser;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import pe.edu.utp.filebrowser.Controllers.FileBrowserController;
@@ -10,15 +11,17 @@ import pe.edu.utp.filebrowser.Enums.Section;
 import pe.edu.utp.filebrowser.Utilites.JavaFXGlobalExceptionHandler;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Objects;
 
 public class FileBrowser extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        //Thread.setDefaultUncaughtExceptionHandler(new JavaFXGlobalExceptionHandler());
+    public void start(Stage stage) throws IOException, URISyntaxException {
+        Thread.setDefaultUncaughtExceptionHandler(new JavaFXGlobalExceptionHandler());
 
         FXMLLoader fxmlLoader = new FXMLLoader(FileBrowser.class.getResource("FileBrowser.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1077, 620);
+        Image icon = new Image(String.valueOf(Objects.requireNonNull(FileBrowser.class.getResource("imgs/Application.png")).toURI()));
         FileBrowserController controller = fxmlLoader.getController();
 
         scene.getStylesheets().add(Objects
@@ -33,6 +36,7 @@ public class FileBrowser extends Application {
         stage.setOnCloseRequest(_ -> controller.quit());
         stage.setTitle("File Browser");
         stage.setScene(scene);
+        stage.getIcons().add(icon);
         stage.setResizable(false);
 
         controller.setStage(stage);
